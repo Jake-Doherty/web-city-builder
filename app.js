573ea1b67cd16d0b70323b43de8d7a1197276aea
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-15 09:03:22 -0700

    add DOM elements for builder side and city state object console.logs are to check if grabbed correctly

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 /* Imports */
 
 /* Get DOM Elements */
+const nameInput = document.getElementById('city-name');
+const climateSelect = document.getElementById('climate-selector');
+const archSelect = document.getElementById('architecture-selector');
+const attrInput = document.getElementById('attractions-input');
 
 /* State */
+const city = {
+    name: 'Keats',
+    climate: 'Temperate',
+    architecture: 'Classical',
+    attractions: ['Tesla Trees', 'The Sphinx'],
+};
+
+console.log(nameInput);
+console.log(climateSelect);
+console.log(archSelect);
+console.log(attrInput);
+console.log(city);
 
 /* Events */
 
